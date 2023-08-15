7e7a6db736de6b12070bf23e96ff697a6722e334
Jad95 <amjadabdullah7667@gmail.com>
2023-08-15 03:46:37 -0400

    assignment files are added

---
+++ Leaflet-Part-2/static/js/logic.js
@@ -0,0 +1,109 @@
+// Define API endpoints for earthquake and plate data
+let earthquakeAPI = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+let plateUrl = "static/data/PB2002_boundaries.json";
+
+// Perform GET requests for earthquake and plate data
+Promise.all([d3.json(earthquakeAPI), d3.json(plateUrl)])
+    .then(function(dataArray) {
+        let earthquakeData = dataArray[0];
+        let plateData = dataArray[1];
+        console.log(earthquakeData);
+        console.log(plateData);
+        createFeatures(earthquakeData.features, plateData.features);
+    });
+
+// Create markers with size and color for earthquakes
+function createMarker(feature, latlng) {
+    return L.circleMarker(latlng, {
+        radius: markerSize(feature.properties.mag),
+        fillColor: markerColor(feature.geometry.coordinates[2]),
+        color: "#000",
+        weight: 0.5,
+        opacity: 0.5,
+        fillOpacity: 1
+    });
+}
+
+function createFeatures(earthquakeData, plateData) {
+    // Create popups for earthquakes
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>Location:</h3> ${feature.properties.place}<h3> Magnitude:</h3> ${feature.properties.mag}<h3> Depth:</h3> ${feature.geometry.coordinates[2]}`);
+    }
+
+    // Create GeoJSON layers for earthquakes and plates
+    let earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: createMarker
+    });
+    let plates = L.geoJSON(plateData, {
+        style: function() {
+            return {
+                color: "blue",
+                weight: 2.5
+            };
+        }
+    });
+
+    // Create map with base and overlay layers
+    createMap(earthquakes, plates);
+}
+
+function createMap(earthquakes, plates) {
+    // Create base layers
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+
+    // Create base maps
+    let baseMaps = {
+        "Street Map": street,
+        "Topographic Map": topo
+    };
+
+    // Create overlay maps
+    let overlayMaps = {
+        "Earthquakes": earthquakes,
+        "Fault Lines": plates
+    };
+
+    // Create map with layers
+    let myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 5,
+        layers: [street, earthquakes, plates]
+    });
+
+    // Create and add legend
+    let legend = L.control({ position: 'bottomright' });
+    legend.onAdd = function (myMap) {
+        let div = L.DomUtil.create('div', 'info legend'),
+            grades = [-10, 10, 30, 60, 90],
+            legendInfo = "<h5>Magnitude</h5>";
+
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                `<i style="background:${markerColor(grades[i] + 1)}"></i> ${grades[i]}${grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+'}`;
+        }
+
+        return div;
+    };
+    legend.addTo(myMap);
+}
+
+// Calculate marker size based on magnitude
+function markerSize(magnitude) {
+    return magnitude * 5;
+}
+
+// Determine marker color based on depth
+function markerColor(depth) {
+    return depth > 90 ? '#d73027' :
+           depth > 70 ? '#fc8d59' :
+           depth > 50 ? '#fee08b' :
+           depth > 30 ? '#d9ef8b' :
+           depth > 10 ? '#91cf60' :
+                        '#1a9850';
+}
--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # leaflet-challenge
+
+## Background Information
+
+The United States Geological Survey, known as USGS, plays a crucial role in providing scientific insights into natural hazards, ecosystem health, environmental conditions, and the effects of climate and land-use changes. Their dedicated scientists develop innovative methods and tools to deliver timely, pertinent, and valuable information about our planet's processes. As a new team member, you will be contributing to an exciting and impactful project.
+
+USGS is embarking on an endeavor to create a new set of visualization tools. These tools aim to help them present their extensive earthquake data in a more meaningful way. Despite collecting a substantial volume of global data on a daily basis, they lack an effective means of displaying it. By visualizing this data, they hope to enhance public awareness, educate other government entities, and potentially secure increased support for addressing critical planetary challenges.
+
+## Website Link
+
+
+## Project Overview
+
+* **Part 1: Crafting the Earthquake Visualization**
+
+* **Part 2: Expanding Data Insights (Optional)**
+
+### Part 1: Crafting the Earthquake Visualization
+
+1. **Collect Earthquake Data:**
+
+   * The USGS offers earthquake data in various formats, regularly updated every five minutes. [USGS GeoJSON Feed](http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php)
+
+   * The dataset "All Earthquakes from the Past 7 Days" provides a JSON representation of this data. The URL for this JSON was utilized to fetch data for our visualization.
+
+2. **Import and Visualize the Data:**
+
+   * We employed Leaflet to design a map plotting all earthquakes from the dataset based on their geographical coordinates.
+
+   * The data markers adjust in size to represent earthquake magnitude, and their color indicates depth. Larger markers signify higher magnitudes, while darker colors indicate greater depths.
+
+   * Interactive popups furnish key details about each earthquake upon clicking a respective marker. Information includes location, magnitude, and depth.
+
+   * A user-friendly legend accompanies the map to offer essential context for interpreting the data.
+
+- - -
+
+### Part 2: Expanding Data Insights
+
+The USGS has requested an additional dataset integration to illustrate the connection between tectonic plates and seismic activity. Your task involves fetching this dataset and visualizing it alongside the original data. The tectonic plates data can be accessed from <https://github.com/fraxen/tectonicplates>.
+
+In Part 2, the following actions were taken:
+
+* Incorporating the tectonic plates dataset onto the map alongside the earthquake data.
+
+* Enabling the addition of various base map options for user selection.
+
+* Organizing the datasets into distinct overlays, permitting independent activation and deactivation.
+
+* Implementing layer controls within the map interface.
+
+## Resources
+
+* [ColorBrewer](https://colorbrewer2.org/)
+* [USGS GeoJSON Feed](http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php)
+* [Tectonic Plates Data](https://github.com/fraxen/tectonicplates)
+* Assignment 15 Instructions
